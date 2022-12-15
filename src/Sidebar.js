c77bee874bbb418b404d7fed4c49dacea4651efa
st173496 <st173496@stud.uni-stuttgart.de>
2022-12-15 23:49:22 +0100

    Initial structure

---
+++ src/Sidebar.js
@@ -0,0 +1,27 @@
+function Sidebar({ notes, onAddNote }) {
+    return (
+        <div className="app-sidebar">
+            <div className="app-sidebar-header">
+                <h1>Notes</h1>
+                <button onClick={onAddNote}>Add</button>
+            </div>
+            <div className="app-sidebar-notes">
+                {notes.map((note) => (
+                    <div className="app-sidebar-note">
+                        <div className="sidebar-note-title">
+                            <strong>{note.title}</strong>
+                            <button>Delete</button>
+                        </div>
+
+                        <p>{note.body && note.body.substr(0, 100) + "..."}</p>
+
+                        <small className="note-meta">{note.lastModified}</small>
+
+                    </div>
+                ))}
+            </div>    
+        </div>
+    );
+}
+
+export default Sidebar;
